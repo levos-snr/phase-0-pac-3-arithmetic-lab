2d5f4fa70c57dfe019a3ea8c239f18de172dc8af
Lewis Odero <lewisodero27@gmail.com>
2024-06-25 00:23:39 +0300

    fix: fix the test case for operators and parse function

---
+++ index.js
@@ -0,0 +1,66 @@
+function add() {
+  return 1 + 2;
+}
+
+add();
+
+function subtract() {
+  return 3 - 1;
+}
+subtract();
+
+function multiply() {
+  return 3 * 4;
+}
+
+function divide() {
+  return 3 / 4;
+}
+
+divide();
+
+function add(a, b) {
+  return a + b;
+}
+
+add(3, 5);
+
+function subtract(a, b) {
+  return a - b;
+}
+
+subtract(7, 2);
+
+function multiply(a, b) {
+  return a * b;
+}
+multiply(3, 5);
+
+function divide(a, b) {
+  return a / b;
+}
+
+divide(21, 7);
+
+function increment(n) {
+  return n + 1;
+}
+
+increment(3);
+
+function decrement(n){
+  return n - 1;
+}
+
+decrement(5);
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+makeInt('2');
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
+
+preserveDecimal('3.33');
